ae250179df1e338796eebab3058295d750eb4907
lsj2015-hub <lsj2015@hotmail.com>
2024-03-31 21:06:41 +0800

    title Metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,12 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  // title: 'Blog',
+  title: {
+    absolute: 'Blog',
+  },
+};
+
 const Blog = () => {
   return <h1>My blog</h1>;
 };
